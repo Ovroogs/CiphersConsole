24cfd1181a1e8a8a4da8570470bed1b1f1651606
ovroogs <saert.tv@gmail.com>
2021-10-23 22:15:57 +0300

    Начало проекта

--- CiphersConsole/Program.cs
+++ CiphersConsole/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using static System.Console;
+
+using CiphersLibrary;
+
+Cipher cipher = null;
+byte on = 0;
+while (on != 9)
+{
+    if (on == 0)
+    {
+        Write("Введите строку: ");
+        cipher = new(ReadLine());
+    }
+    while (true)
+    {
+        Write("0. Другая строка для шифровки\n1. Шифр Виженера(шифровка)\n2. Шифр Виженера(дешифровка)\n3. Шифр Цезаря(шифровка)\n4. Шифр Цезаря(дешифровка)\n" +
+              "5. Транспозиция(шифровка\\дешифровка)\n6. Xor(шифровка\\дешифровка).\n7. Шифр Плейфера(шифровка)\n8. Шифр Плейфера(дешифровка)\n9. Выход\n");
+        Write("Введите пункт: ");
+        if (byte.TryParse(ReadLine(), out on) && on >= 0 && on < 10)
+            break;
+        Clear();
+    }
+
+    cipher.ChoiceCipher(on);
+}
--- CiphersLibrary/Cipher.cs
+++ CiphersLibrary/Cipher.cs
@@ -1,7 +1,54 @@
-﻿namespace CiphersLibrary
+﻿using static System.Console;
+
+namespace CiphersLibrary
 {
     public class Cipher
     {
+        private string _stringToEncrypt;
 
+        public Cipher(string str)
+        {
+            _stringToEncrypt = str;
+        }
+
+        public void ChoiceCipher(byte number)
+        {
+            switch (number)
+            {
+                case 1: VigenereСipher(_stringToEncrypt.ToUpper()); break;
+                case 2: VigenereСipher(_stringToEncrypt.ToUpper()); break;
+                case 3: CaesarСipher(_stringToEncrypt.ToUpper()); break;
+                case 4: CaesarСipher(_stringToEncrypt.ToUpper()); break;
+                case 5: TranspositionСipher(_stringToEncrypt); break;
+                case 6: XorСipher(_stringToEncrypt); break;
+                case 7: PlayfairСipher(_stringToEncrypt.Replace(" ", string.Empty).ToUpper().Replace("J", "I")); break;
+                case 8: PlayfairСipher(_stringToEncrypt.Replace(" ", string.Empty).ToUpper().Replace("J", "I")); break;
+            }
+        }
+
+        public void CaesarСipher(string str)
+        {
+
+        }
+
+        public void VigenereСipher(string str)
+        {
+
+        }
+
+        public void TranspositionСipher(string str)
+        {
+
+        }
+
+        public void XorСipher(string str)
+        {
+
+        }
+
+        public void PlayfairСipher(string str)
+        {
+
+        }
     }
 }
