a08b6a0720126f411153e633e4af54085ae1d40c
ovroogs <saert.tv@gmail.com>
2021-10-24 02:20:04 +0300

    Реализация шифра цезаря

--- CiphersConsole/Program.cs
+++ CiphersConsole/Program.cs
@@ -2,24 +2,30 @@
 
 using CiphersLibrary;
 
+
 Cipher cipher = null;
-byte on = 0;
-while (on != 9)
+byte numberCipher = 0;
+
+while (true)
 {
-    if (on == 0)
+    if (numberCipher == 0)
     {
         Write("Введите строку: ");
         cipher = new(ReadLine());
     }
+
     while (true)
     {
-        Write("0. Другая строка для шифровки\n1. Шифр Виженера(шифровка)\n2. Шифр Виженера(дешифровка)\n3. Шифр Цезаря(шифровка)\n4. Шифр Цезаря(дешифровка)\n" +
+        WriteLine("0. Другая строка для шифровки\n1. Шифр Виженера(шифровка)\n2. Шифр Виженера(дешифровка)\n3. Шифр Цезаря(шифровка)\n4. Шифр Цезаря(дешифровка)\n" +
               "5. Транспозиция(шифровка\\дешифровка)\n6. Xor(шифровка\\дешифровка).\n7. Шифр Плейфера(шифровка)\n8. Шифр Плейфера(дешифровка)\n9. Выход\n");
         Write("Введите пункт: ");
-        if (byte.TryParse(ReadLine(), out on) && on >= 0 && on < 10)
+        if (byte.TryParse(ReadLine(), out numberCipher) && numberCipher >= 0 && numberCipher < 10)
+        {
+            Clear();
+            WriteLine(cipher.ChoiceCipher(numberCipher));
             break;
-        Clear();
+        }
     }
 
-    cipher.ChoiceCipher(on);
+    if (numberCipher == 9) break;
 }
--- CiphersLibrary/Cipher.cs
+++ CiphersLibrary/Cipher.cs
@@ -1,54 +1,118 @@
-﻿using static System.Console;
+﻿using System.Text.RegularExpressions;
+
+using static System.Console;
 
 namespace CiphersLibrary
 {
     public class Cipher
     {
         private string _stringToEncrypt;
+        private const int symbolMin = 65, symbolMax = 90, AlphabetSize = 26;
 
         public Cipher(string str)
         {
             _stringToEncrypt = str;
         }
 
-        public void ChoiceCipher(byte number)
+        public string ChoiceCipher(byte number)
         {
-            switch (number)
+            return number switch
             {
-                case 1: VigenereСipher(_stringToEncrypt.ToUpper()); break;
-                case 2: VigenereСipher(_stringToEncrypt.ToUpper()); break;
-                case 3: CaesarСipher(_stringToEncrypt.ToUpper()); break;
-                case 4: CaesarСipher(_stringToEncrypt.ToUpper()); break;
-                case 5: TranspositionСipher(_stringToEncrypt); break;
-                case 6: XorСipher(_stringToEncrypt); break;
-                case 7: PlayfairСipher(_stringToEncrypt.Replace(" ", string.Empty).ToUpper().Replace("J", "I")); break;
-                case 8: PlayfairСipher(_stringToEncrypt.Replace(" ", string.Empty).ToUpper().Replace("J", "I")); break;
+                1 => VigenereСipher(_stringToEncrypt.ToUpper(), EncryptionMode.Encryption),
+                2 => VigenereСipher(_stringToEncrypt.ToUpper(), EncryptionMode.Decryption),
+                3 => CaesarСipher(_stringToEncrypt.ToUpper(), EncryptionMode.Encryption),
+                4 => CaesarСipher(_stringToEncrypt.ToUpper(), EncryptionMode.Decryption),
+                5 => TranspositionСipher(_stringToEncrypt),
+                6 => XorСipher(_stringToEncrypt),
+                7 => PlayfairСipher(_stringToEncrypt.Replace(" ", string.Empty).ToUpper().Replace("J", "I")),
+                8 => PlayfairСipher(_stringToEncrypt.Replace(" ", string.Empty).ToUpper().Replace("J", "I")),
+                _ => "",
+            };
+        }
+
+        private string CaesarСipher(string originString, EncryptionMode mode)
+        {
+            var cipherString = string.Empty;
+            CreateKey(out byte key);
+
+            foreach (var characterFromString in originString)
+            {
+                var character = characterFromString;
+                if (character >= symbolMin && character <= symbolMax)
+                {
+                    var isEncryption = EncryptionMode.Encryption == mode;
+                    var border = isEncryption ? symbolMax : symbolMin;
+                    var encKey = isEncryption ? key : -key;
+
+                    Enc(ref character, encKey, border, AlphabetSize, isEncryption);
+                }
+
+                cipherString += character;
+            }
+
+            return $"{originString} {cipherString}";
+        }
+
+        private string VigenereСipher(string originString, EncryptionMode mode)
+        {
+            return "";
+        }
+
+        private string TranspositionСipher(string str)
+        {
+            return "";
+        }
+
+        private string XorСipher(string str)
+        {
+            return "";
+        }
+
+        private string PlayfairСipher(string str)
+        {
+            return "";
+        }
+
+
+        private void Enc(ref char character, int key, int border, int alphabetSize, bool isEncryption)
+        {
+            character = (char)(character + key);
+            if (isEncryption)
+            {
+                if (character > border) character = (char)(character - alphabetSize); 
+            }
+            else
+            {
+                if (character < border) character = (char)(character + alphabetSize);
             }
         }
 
-        public void CaesarСipher(string str)
-        {
 
+        private void CreateKey(out string key)
+        {
+            while (true)
+            {
+                Write("Введите ключ(только буквы английского алфавита): ");
+                key = ReadLine();
+                if (!key.Contains(' ') && Regex.IsMatch(key, @"[A-Za-z]", RegexOptions.IgnoreCase)) break;
+            }
+            Clear();
         }
 
-        public void VigenereСipher(string str)
+        private void CreateKey(out byte key)
         {
-
+            while (true)
+            {
+                Write("Введите ключ(только цифры): ");
+                if (byte.TryParse(ReadLine(), out key)) break;
+            }
+            Clear();
         }
+    }
 
-        public void TranspositionСipher(string str)
-        {
-
-        }
-
-        public void XorСipher(string str)
-        {
-
-        }
-
-        public void PlayfairСipher(string str)
-        {
-
-        }
+    public enum EncryptionMode
+    {
+        Encryption,
+        Decryption
     }
 }
